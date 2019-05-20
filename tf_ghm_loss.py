c2a25e200f9e61dd8e9f7193c82eb93231258573
Kuan-Yu Huang <peter124574@gmail.com>
2019-05-20 11:59:48 +0800

    Update tf_ghm_loss.py
    remove tf.while_loop to avoid tf graph issue and speed up.

--- tf_ghm_loss.py
+++ tf_ghm_loss.py
@@ -1,9 +1,10 @@
 """TF version for grandient harmonized weights"""
 
+import numpy as np
 import tensorflow as tf
 
 
-def GHM_weight(predict, target, valid_mask, bins=10, alpha=0.75,
+def get_ghm_weight(predict, target, valid_mask, bins=10, alpha=0.75,
                dtype=tf.float32, name='GHM_weight'):
     """ Get Grandient Harmonized Weights.
     This is an implementation of the GHM ghm_weights described
@@ -36,8 +37,8 @@ def GHM_weight(predict, target, valid_mask, bins=10, alpha=0.75,
         edges = tf.constant(_edges, dtype=dtype)
 
         _shape = predict.get_shape().as_list()
-        assert len(_shape) == 2
-        _init_statistics = (_shape[0] * _shape[1]) / bins
+
+        _init_statistics = (np.prod(_shape)) / bins
         statistics = tf.get_variable(
             name='statistics', shape=[bins], dtype=dtype, trainable=False,
             initializer=tf.constant_initializer(_init_statistics, dtype=dtype))
@@ -46,37 +47,41 @@ def GHM_weight(predict, target, valid_mask, bins=10, alpha=0.75,
         total = tf.maximum(tf.reduce_sum(tf.cast(_b_valid, dtype=dtype)), 1)
 
         grandients = tf.abs(predict - target)
-        weights = tf.zeros_like(predict)
 
         # Calculate new statics and new weights
-        cond = lambda i, weights_loop, statistics_loop: i < bins
-        def body(i, weights_loop, statistics_loop):
+        w_list = []
+        s_list = []
+        for i in range(bins):
             inds = (
                 grandients >= edges[i]) & (grandients < edges[i + 1]) & _b_valid
             # number of examples lying in bin, same as R in paper.
             num_in_bin = tf.reduce_sum(tf.cast(inds, dtype=dtype))
-            statistics_loop += tf.one_hot(i, bins) * (
-                (1 - alpha) * (num_in_bin - statistics_loop))
-            grandient_density = statistics_loop[i] * bins
+            statistics_i = alpha * statistics[i] + (1 - alpha) * num_in_bin
+            grandient_density = statistics_i * bins
             update_weights = total / grandient_density
-            weights_loop = tf.where(inds,
-                x=tf.ones_like(weights_loop) * update_weights, y=weights_loop)
-            return i + 1, weights_loop, statistics_loop
-        _, weights, new_s = tf.while_loop(cond, body, [0, weights, statistics])
+            weights_i = tf.where(
+                inds,
+                x=tf.ones_like(predict) * update_weights,
+                y=tf.zeros_like(predict))
+            w_list.append(weights_i)
+            s_list.append(statistics_i)
+        
+        weights = tf.add_n(w_list)
+        new_statistics = tf.stack(s_list)
 
         # Avoid the tiny value in statistics
-        new_s = tf.maximum(new_s, _init_statistics * 1e-6)
+        new_statistics = tf.maximum(new_statistics, _init_statistics * 1e-6)
         # Update statistics
-        statistics_updated_op = statistics.assign(new_s)
+        statistics_updated_op = statistics.assign(new_statistics)
         tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, statistics_updated_op)
 
     return weights
 
 
 def main():
-    ghm_weights = GHM_weight(predict=tf.constant([[1., 0., 0.5, 0.]]),
-                             target=tf.constant([[1., 0., 0., 1.]]),
-                             valid_mask=tf.constant([[1., 1., 1., 1.]]))
+    ghm_weights = get_ghm_weight(predict=tf.constant([[1., 0., 0.5, 0.]]),
+                                 target=tf.constant([[1., 0., 0., 1.]]),
+                                 valid_mask=tf.constant([[1., 1., 1., 1.]]))
 
     # update method same as the batch norm update with optimizer
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
